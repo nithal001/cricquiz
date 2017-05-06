29a17788713415f4b94d55bffdd866e6cf6d2825
nithal <nithalajay@gmail.com>
2017-05-06 16:00:53 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A Cricket Quiz App
---
+++ src/app/home-carousel/home-carousel.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'home-carousel',
+  templateUrl: './home-carousel.component.html'
+})
+export class HomeCarouselComponent {}
